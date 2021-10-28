c94f0911f4dc2cd22da283aabf3b8517425f3a40
Danil Cherepanov <dancheran@yandex.ru>
2021-10-28 22:38:09 +0300

    Added unit tests for ip filter

--- ip_filter/ip_filter.h
+++ ip_filter/ip_filter.h
@@ -1,7 +1,7 @@
+#pragma once
+
 #include <algorithm>
 #include <array>
-#include <cassert>
-#include <cstdlib>
 #include <iostream>
 #include <numeric>
 #include <sstream>
@@ -9,28 +9,7 @@
 #include <string>
 #include <vector>
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d) {
-  std::vector<std::string> r;
-
-  std::string::size_type start = 0;
-  std::string::size_type stop = str.find_first_of(d);
-  while (stop != std::string::npos) {
-    r.push_back(str.substr(start, stop - start));
-
-    start = stop + 1;
-    stop = str.find_first_of(d, start);
-  }
-
-  r.push_back(str.substr(start));
-
-  return r;
-}
+#include "utils.h"
 
 class IPAddress {
 public:
@@ -60,14 +39,14 @@ public:
     }
   }
 
-  template <size_t Index> uint8_t Get() const noexcept {
+  template <size_t Index>[[nodiscard]] uint8_t Get() const noexcept {
     static_assert(Index < Size,
                   "Index must be not greater than IPAddress::Size");
 
     return m_data[Index];
   }
 
-  std::string ToString() const {
+  [[nodiscard]] std::string ToString() const {
     std::stringstream result;
     bool first = true;
 
@@ -84,14 +63,22 @@ public:
     return result.str();
   }
 
-  friend bool operator>(const IPAddress &lhs, const IPAddress &rhs) noexcept;
+  friend bool
+  operator>(const IPAddress &lhs, const IPAddress &rhs) noexcept;
+  friend bool operator==(const IPAddress &lhs, const IPAddress &rhs) noexcept;
 
 private:
   std::array<uint8_t, Size> m_data;
 };
 
-bool operator>(const IPAddress &lhs, const IPAddress &rhs) noexcept {
+[[nodiscard]] bool operator>(const IPAddress &lhs,
+                             const IPAddress &rhs) noexcept {
   return lhs.m_data > rhs.m_data;
+}
+
+[[nodiscard]] bool operator==(const IPAddress &lhs,
+                              const IPAddress &rhs) noexcept {
+  return lhs.m_data == rhs.m_data;
 }
 
 std::ostream &operator<<(std::ostream &out, const IPAddress &ip) {
@@ -100,9 +87,10 @@ std::ostream &operator<<(std::ostream &out, const IPAddress &ip) {
 
 using IPList = std::vector<IPAddress>;
 
+namespace detail {
 template <typename... Args, std::size_t... I>
-IPList filterImpl(const IPList &ipSet, std::index_sequence<I...>,
-                  const Args &... args) {
+[[nodiscard]] IPList filterImpl(const IPList &ipSet, std::index_sequence<I...>,
+                                const Args &... args) {
   IPList result;
   for (const auto &ip : ipSet) {
     if (((ip.Get<I>() == args) && ...)) {
@@ -114,8 +102,8 @@ IPList filterImpl(const IPList &ipSet, std::index_sequence<I...>,
 }
 
 template <typename... Args, std::size_t... I>
-IPList filterAnyImpl(const IPList &ipSet, uint8_t ipPart,
-                     std::index_sequence<I...>) {
+[[nodiscard]] IPList filterAnyImpl(const IPList &ipSet, uint8_t ipPart,
+                                   std::index_sequence<I...>) {
   IPList result;
   for (const auto &ip : ipSet) {
     if (((ip.Get<I>() == ipPart) || ...)) {
@@ -125,54 +113,19 @@ IPList filterAnyImpl(const IPList &ipSet, uint8_t ipPart,
 
   return result;
 }
+} // namespace detail
 
 template <typename... Args>
-IPList filter(const IPList &ipSet, const Args &... args) {
+[[nodiscard]] IPList filter(const IPList &ipSet, const Args &... args) {
   constexpr size_t ArgsCount = sizeof...(Args);
   static_assert(ArgsCount > 0 && ArgsCount <= 4,
                 "Count of filter arguments must be > 0 and <= 4.");
-  return filterImpl(ipSet, std::make_index_sequence<ArgsCount>(), args...);
+  return detail::filterImpl(ipSet, std::make_index_sequence<ArgsCount>(),
+                            args...);
 }
 
 template <typename... Args>
-IPList filterAny(const IPList &ipSet, uint8_t ipPart) {
-  return filterAnyImpl(ipSet, ipPart,
-                       std::make_index_sequence<IPAddress::Size>());
-}
-
-int main(int argc, char const *argv[]) try {
-
-  IPList ipPool;
-
-  for (std::string line; std::getline(std::cin, line);) {
-    std::vector<std::string> v = split(line, '\t');
-    ipPool.emplace_back(v.at(0));
-  }
-
-  // reverse lexicographically sort
-  std::sort(ipPool.begin(), ipPool.end(), std::greater{});
-
-  for (const auto &ip : ipPool) {
-    std::cout << ip << std::endl;
-  }
-
-  // filter by first byte and output
-  for (const auto &ip : filter(ipPool, 1)) {
-    std::cout << ip << std::endl;
-  }
-
-  // filter by first and second bytes and output
-  for (const auto &ip : filter(ipPool, 46, 70)) {
-    std::cout << ip << std::endl;
-  }
-
-  // filter by first and second bytes and output
-  for (const auto &ip : filterAny(ipPool, 46)) {
-    std::cout << ip << std::endl;
-  }
-
-  return 0;
-} catch (const std::exception &e) {
-  std::cerr << e.what() << std::endl;
-  return 1;
+[[nodiscard]] IPList filterAny(const IPList &ipSet, uint8_t ipPart) {
+  return detail::filterAnyImpl(ipSet, ipPart,
+                               std::make_index_sequence<IPAddress::Size>());
 }
